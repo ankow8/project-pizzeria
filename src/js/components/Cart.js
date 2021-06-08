29316d8b1a44a8a9a45217844f6a79e6960adfa4
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-06-08 19:09:24 +0200

    Add function switch pages

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,7 +1,6 @@
 import {select, classNames, settings, templates} from '../settings.js';
 import utils from '../utils.js';
-import CartProduct from './CartProduct.js'; // trzeba bylo usunac wedlug konsoli folder components
-
+import CartProduct from './CartProduct.js';
 
 class Cart {
   constructor(element){
